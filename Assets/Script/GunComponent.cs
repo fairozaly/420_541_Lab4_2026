cf8833c3d67ccaff8127a1c2770065c0b947b331
Fairoz Aly <fairozaly9@gmail.com>
2026-09-18 10:43:51 -0400

    lab4

--- Assets/Script/GunComponent.cs
+++ Assets/Script/GunComponent.cs
@@ -4,10 +4,43 @@ public class GunComponent : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawnPoint;
-    public float bulletMaxImpulse = 10.0f;
+    public float bulletMaxImpulse = 100.0f;
     public float maxChargeTime = 3.0f;
     private float chargeTime = 0.0f;
     private bool isCharging = false;
 
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            chargeTime = 0.0f;
+            isCharging = true;
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            chargeTime += Time.deltaTime;
+            chargeTime = Mathf.Clamp(chargeTime, 0 , maxChargeTime);
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ShootBullet();
+            isCharging = false;
+        }
+    }
+
    
+    void ShootBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+       
+        // TODO change that equation so that it adds an impulse that follows charge time
+        float bulletImpulse = (chargeTime / maxChargeTime) * bulletMaxImpulse;
+
+        // An impulse is a force you apply on a object in a single instant.
+        rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+    }
 }
+
